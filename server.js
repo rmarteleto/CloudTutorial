65e832a12421cb0c6c0e9b78e784a1c2ffb0825b
Marteleto, Renato <Renato.Marteleto@elekta.com>
2015-04-22 12:40:20 -0700

    update server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 'use strict';
 var http = require('http')
 var port = process.env.PORT || 1337;
+console.log('Starting up - port is:' + port);
+var resCount = 0;
 http.createServer(function(req, res) {
- res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello World\n');
+console.log('Response count:' + ++resCount);
+res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.end('Hello World\n');
+console.log('Sent response to client.');
 }).listen(port);
