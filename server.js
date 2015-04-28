4ca4712583c502be36d254e96aa9e506228939a4
Marteleto, Renato <Renato.Marteleto@elekta.com>
2015-04-28 12:41:29 -0700

    Convert raw http to hapi.js server.

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 'use strict';
-var http = require('http')
-var port = process.env.PORT || 1337;
-console.log('Starting up - port is:' + port);
-var resCount = 0;
-http.createServer(function(req, res) {
-console.log('Response count:' + ++resCount);
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.end('Hello World\n');
-console.log('Sent response to client.');
-}).listen(port);
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+if( process.env.PORT ) {
+  server.connection({ port: process.env.PORT });
+}
+else {
+  server.connection({ port: 1337 });
+}
+
+// add a route
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+// start the server
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
